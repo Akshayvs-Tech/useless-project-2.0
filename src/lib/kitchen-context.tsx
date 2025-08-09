54ccd919b6a017b35af87ffae42e107176ed1193
vedhavk <vedhavk2004@gmail.com>
2025-08-09 17:29:33 +0530

    Changed the text

--- src/lib/kitchen-context.tsx
+++ src/lib/kitchen-context.tsx
@@ -1,4 +1,5 @@
 'use client';
+/* eslint-disable react/no-unescaped-entities */
 
 import React, { createContext, useContext, useReducer, ReactNode } from 'react';
 import { KitchenState, Ingredient, Recipe, CookingItem } from '@/types/kitchen';
